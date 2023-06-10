ebc3fce8f27bb8ebab4244448a7c1230801f2e6f
TabasumNad <tmnadaf17@gmail.com>
2023-06-10 21:26:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+const app = express();
+import fs from 'fs';
+
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to file System Task!');
+});
+
+app.get('/create', (request, response) => {
+  var content = new Date().getTime().toLocaleString();
+
+  var dateTime = `${new Date().getDate()}-${
+    new Date().getMonth() + 1
+  }-${new Date().getFullYear()}-${new Date().getHours()}-${new Date().getMinutes()}-${new Date().getSeconds()}`;
+
+  fs.writeFile(`./${dateTime}.txt`, content, (err) => {
+    if (err) console.log(err);
+    else console.log('success');
+  });
+  response.send('files created');
+});
+
+app.get('/files', (request, response) => {
+  fs.readdir('./', (err, Files) => {
+    if (err) console.log(err);
+    else response.json({ Files });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`The Server Started at the port :  ${port}`);
+});
